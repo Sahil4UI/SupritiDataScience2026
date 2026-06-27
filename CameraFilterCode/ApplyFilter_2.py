0dffd2792c53b373548bf2b7e7236dd077f76b22
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2026-06-27 10:30:17 +0530

    Add files via upload

---
+++ CameraFilterCode/ApplyFilter_2.py
@@ -0,0 +1,42 @@
+import cv2
+
+dataset = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# mask image
+s_img = cv2.imread("mask_1.png",-1)
+capture = cv2.VideoCapture(0)
+
+while True:
+    # l img - camera
+    ret, l_img = capture.read()
+    # coverting it into GreyScale /black and white
+    gray = cv2.cvtColor(l_img, cv2.COLOR_BGR2GRAY)
+    faces = dataset.detectMultiScale(gray, 1.2)
+    
+    if len(faces) > 0:
+
+        # For Cap
+        # x_offset = faces[0][0] - 15
+        # y_offset = faces[0][1] - s_img.shape[1]
+
+        # For Mask
+        x_offset = faces[0][0]
+        y_offset = faces[0][1]
+
+        y1, y2 = y_offset, y_offset + s_img.shape[0]
+        x1, x2 = x_offset, x_offset + s_img.shape[1]
+
+        alpha_s = s_img[:, :, 3] / 255.0
+        alpha_l = 1.0 - alpha_s
+
+        for c in range(0, 3):
+            l_img[y1:y2, x1:x2, c] = (alpha_s * s_img[:, :, c] + alpha_l * l_img[y1:y2, x1:x2, c])
+        # for x, y, w, h in faces:
+        #     l_img[y:y + s_img.shape[0], x:x + s_img.shape[1]] = s_img
+
+        cv2.imshow('result',l_img)
+    if cv2.waitKey(1) == 27:
+        break
+
+capture.release()
+cv2.destroyAllWindows()
